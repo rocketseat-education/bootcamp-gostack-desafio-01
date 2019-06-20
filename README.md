3e5e265695b761effb5864ee71c645414ea97caa
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-20 10:36:26 -0300

    Desafio

---
+++ README.md
@@ -0,0 +1,45 @@
+# Desafio 01. Conceitos do NodeJS
+
+Crie uma aplicação do zero utilizando Express.
+
+Essa aplicação será utilizada para armazenar projetos e suas tarefas.
+
+## Rotas
+
+- `POST /projects`: A rota deve receber `id` e `title` dentro corpo de cadastrar um novo projeto dentro de um array no seguinte formato: `{ id: 1, title: 'Novo projeto', tasks: [] }`;
+
+- `GET /projects`: Rota que lista todos projetos e suas tarefas;
+
+- `PUT /projects/:id`: A rota deve alterar apenas o título do projeto com o `id` presente nos parâmetros da rota;
+
+- `DELETE /projects/:id`: A rota deve deletar o projeto com o `id` presente nos parâmetros da rota;
+
+- `POST /projects/:id/tasks`: A rota deve receber um campo `title` e armazenar uma nova tarefa no array de tarefas de um projeto específico escolhido através do `id` presente nos parâmetros da rota;
+
+### Exemplo
+
+Se eu chamar a rota `POST /projects` repassando `{ id: 1, title: 'Novo projeto' }` e a rota `POST /projects/1/tasks` com `{ title: 'Nova tarefa' }`, meu array de projetos deve ficar assim:
+
+```js
+[
+  {
+    id: 1,
+    title: 'Novo projeto',
+    tasks: ['Nova tarefa']
+  }
+]
+```
+
+## Middlewares
+
+- Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+
+- Crie um middleware global chamado em todas requisições que imprime (`console.log`) uma contagem de quantas requisições foram feitas na aplicação até então;
+
+## Entrega
+
+Esse desafio **não precisa ser entregue** e não receberá correção, mas você pode ver o resultado do código do desafio aqui: https://github.com/Rocketseat/bootcamp-desafio-01
+
+Após concluir o desafio, adicionar esse código ao seu Github é uma boa forma de demonstrar seus conhecimentos para oportunidades futuras.
+
+“Sua única limitação é você mesmo”!
---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+/**
+ * Utilizamos a variável `numberOfRequests` como
+ * `let` porque vai sofrer mutação. A variável 
+ * `projects` pode ser `const` porque um `array`
+ * pode receber adições ou exclusões mesmo sendo
+ * uma constante. 
+ */
+let numberOfRequests = 0;
+const projects = [];
+
+/**
+ * Middleware que checa se o projeto existe
+ */
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+/**
+ * Middleware que dá log no número de requisições
+ */
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`Número de requisições: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+/**
+ * Projects
+ */
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+/**
+ * Tasks
+ */
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});;
+
+server.listen(3000);
